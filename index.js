7d39eedb458380712ebf8bf32700571d4b183287
Juan Boubeta <juan.boubeta@uca.es>
2018-04-12 10:41:00 +0000

    First functional API version

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http');
+    
+var cors = require("cors"); // la biblioteca cors permite anadir cabeceras que facilitan el acceso a una API desde cualquier parte
+
+var app = require('express')(); // esta biblioteca permite crear codigo mas sencillo
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
+var serverPort = process.env.PORT || 8080 ;
+
+app.use(cors());
+
+app.options("/*", (req, res, next) => {
+  return res.sendStatus(200);
+});
+
+app.get('/about', function(req, res) {
+  res.redirect("https://pafmon.github.io/rcis-test/");
+});
+
+// swaggerRouter configuration
+var options = {
+  swaggerUi: path.join(__dirname, '/swagger.json'),
+  controllers: path.join(__dirname, './controllers'),
+  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+
+  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+  app.use(middleware.swaggerMetadata());
+
+  // Validate Swagger requests
+  app.use(middleware.swaggerValidator());
+
+  // Route validated requests to appropriate controller
+  app.use(middleware.swaggerRouter(options));
+
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
+
+
+
+
+  // Start the server
+  http.createServer(app).listen(serverPort, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  });
+
+});
